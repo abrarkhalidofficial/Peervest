b93d7f936244eff419213500426ec33f9fd7d3d8
Bilal Chaudhary <bilalchaudhary478@gmail.com>
2021-10-27 01:34:50 +0500

    re design card

--- src/Screens/StartUpScreen.js
+++ src/Screens/StartUpScreen.js
@@ -108,7 +108,7 @@ export default function StartUpScreen() {
             src={start__up__background}
             alt="start__up__background"
             className="homepage__main__container__jumbotron__img"
-            style={{ height: "fit-content", marginTop: -350 }}
+            style={{ height: "fit-content", marginTop: -500 }}
           />
           <div className="homepage__main__container__jumbotron__wrapper">
             <div className="homepage__main__container__jumbotron__wrapper__content">
