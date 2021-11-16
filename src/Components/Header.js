096ad8354d5dfe11cd7beabc3d40775efceb8222
ibilalchaudhary <bilalchaudhary478@gmail.com>
2021-11-16 14:14:48 +0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -121,9 +121,9 @@ export default class Header extends Component {
               <Link to="/services" className="nav__header__link">
                 Services
               </Link>
-              <Link to="/clients" className="nav__header__link">
+              {/* <Link to="/clients" className="nav__header__link">
                 Clients
-              </Link>
+              </Link> */}
               <Link to="/about-us" className="nav__header__link">
                 About Us
               </Link>
